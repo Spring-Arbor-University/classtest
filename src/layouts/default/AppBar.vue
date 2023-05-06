da7cec517c6626c9533abe18d803843ba70e0499
rmelton <rmeltonmI@gmail.com>
2023-05-06 14:09:58 -0400

    initial commit

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,35 @@
+<script   setup>
+//
+import {ref,onMounted} from "vue";
+
+const menuItems = ['menu1','menu2'];
+
+</script>
+
+<template>
+
+    <v-app-bar color="primary" flat>
+      <v-container class="fill-height d-flex align-center">
+        <v-toolbar-title>Application Name</v-toolbar-title>
+
+        <v-btn
+          v-for="link in menuItems"
+          :key="link"
+          variant="text"
+        >
+          {{ link }}
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-responsive max-width="260">
+          <v-text-field
+            density="compact"
+            hide-details
+            variant="solo"
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
+</template>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+
+<script  setup>
+
+import MyUserStoryApp from "@/components/MyUserStoryApp.vue";
+</script>
+
+<template>
+
+  <my-user-story-app/>
+</template>
